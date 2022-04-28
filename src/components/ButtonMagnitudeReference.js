6fbe987236769f8c2e9af6088b5a3a20ad3e7d91
danielglez4234 <gnzlzdaniel.task@gmail.com>
2022-04-28 09:24:36 +0100

    indent changes

--- src/components/ButtonMagnitudeReference.js
+++ src/components/ButtonMagnitudeReference.js
@@ -15,45 +15,45 @@ import ListIcon from '@mui/icons-material/List';
 
 function ButtonMagnitudeReference({magnitudeTitles, magnitudeReferences}) {
 
-    /*
-     * List all magnitude values references 
-     */
-    const listMagnitudeReferences = () => {
-      let references = []
-      // magnitudeTitles.map((name, index) => {
-      //   references.push(
-      //     <div>{name}</div>
-      //   ) 
-      //     magnitudeReferences.map(objects => {
-      //       let fieldsWithValues = Object.entries(objects);
-            
-      //       fieldsWithValues.map(([key, value]) => {
-      //         references.push(
-      //           <div className="references-values-box">
-      //             <div className="references-values-labels">{key}</div>
-      //             <div className="references-values-values">{value}</div>
-      //           </div>
-      //         )
-      //       }) 
-      //     })
-      // })
+	/*
+	 * List all magnitude values references 
+	 */
+	const listMagnitudeReferences = () => {
+		let references = []
+		// magnitudeTitles.map((name, index) => {
+		//   references.push(
+		//     <div>{name}</div>
+		//   ) 
+		//     magnitudeReferences.map(objects => {
+		//       let fieldsWithValues = Object.entries(objects);
+			
+		//       fieldsWithValues.map(([key, value]) => {
+		//         references.push(
+		//           <div className="references-values-box">
+		//             <div className="references-values-labels">{key}</div>
+		//             <div className="references-values-values">{value}</div>
+		//           </div>
+		//         )
+		//       }) 
+		//     })
+		// })
 
-      for (let i = 0; i < magnitudeTitles.length; i++) {
-        references.push(
-          <div className="references-values-sTitle">{magnitudeTitles[i]}</div>
-        ) 
-        let refere = magnitudeReferences[i]
-        let fieldsAndVal = Object.entries(refere)
-        for (let a = 0; a < fieldsAndVal.length; a++) {
-            references.push(
-              <div className="references-values-box">
-                <div className="references-values-labels">{fieldsAndVal[a][0]}</div>
-                <div className="references-values-values">{fieldsAndVal[a][1]}</div>
-              </div>
-            )
-        }
-      }
-      return references
+		for (let i = 0; i < magnitudeTitles.length; i++) {
+			references.push(
+				<div className="references-values-sTitle">{magnitudeTitles[i]}</div>
+			) 
+			let refere = magnitudeReferences[i]
+			let fieldsAndVal = Object.entries(refere)
+			for (let a = 0; a < fieldsAndVal.length; a++) {
+				references.push(
+					<div className="references-values-box">
+						<div className="references-values-labels">{fieldsAndVal[a][0]}</div>
+						<div className="references-values-values">{fieldsAndVal[a][1]}</div>
+					</div>
+				)
+			}
+		}
+		return references
     };
 
     const referencesList = listMagnitudeReferences()
@@ -63,33 +63,33 @@ function ButtonMagnitudeReference({magnitudeTitles, magnitudeReferences}) {
      * Handle graphic options OPEN popover
      */
     const handleClickOpenList = () => {
-      $('.graphOpt-box-magnitude-ref').toggleClass('display-none');
-      $('.close_rangeZone-magnitude-ref').toggleClass('display-none');
+      	$('.graphOpt-box-magnitude-ref').toggleClass('display-none');
+      	$('.close_rangeZone-magnitude-ref').toggleClass('display-none');
     };
 
     return(
-      <div className="button-general-options">
-        <LtTooltip title="Magnitude Values References" placement="top" className="tool-tip-options">
-          <IconButton aria-label="settings" onClick={handleClickOpenList}>
-            <ListIcon className="settings-generalOptButton"/>
-          </IconButton>
-        </LtTooltip>
+		<div className="button-general-options">
+			<LtTooltip title="Magnitude Values References" placement="top" className="tool-tip-options">
+				<IconButton aria-label="settings" onClick={handleClickOpenList}>
+					<ListIcon className="settings-generalOptButton"/>
+				</IconButton>
+			</LtTooltip>
+			
+			<div className="close_rangeZone-magnitude-ref display-none" onClick={handleClickOpenList}></div>
+			<Box className="graphOpt-box-magnitude-ref display-none" id="graphOpt-sx" sx={{boxShadow: 3}}>
+				<div className="graphOpt-label-title">
+					<p>Magnitude References</p>
+				</div>
 
-        <div className="close_rangeZone-magnitude-ref display-none" onClick={handleClickOpenList}></div>
-        <Box className="graphOpt-box-magnitude-ref display-none" id="graphOpt-sx" sx={{boxShadow: 3}}>
-          <div className="graphOpt-label-title">
-            <p>Magnitude References</p>
-          </div>
+				<div className="graphOpt-refrences-box">
+					{
+						referencesList
+					}
+				</div>
 
-          <div className="graphOpt-refrences-box">
-            {
-              referencesList
-            }
-          </div>
-
-          <div className="graphOpt-action-buttons"></div>
-        </Box>
-      </div>
+				<div className="graphOpt-action-buttons"></div>
+			</Box>
+		</div>
     );
 }
 export default ButtonMagnitudeReference;
--- src/components/ComponentElement.js
+++ src/components/ComponentElement.js
@@ -3,21 +3,21 @@ import folder         from '../img/folderIcon.png';
 
 const ComponentElement = ({ title, getMonitors }) => {
   // <p className="componentItem-title"> { title } </p>
-  return(
-    <div className="componentItem-box-container">
-      <div className="componentItem-box" onClick={() => { getMonitors(title) }}>
-        <div className="componentItem-icon component-icon-color">
-          <img src={folder} alt="folder" className="component_IMG-folder"/>
-        </div>
-        <div className="componentItem-title-div">
-          <p className="componentItem-title"> { title } </p>
-        </div>
-{/*        <div className="componentItem-title-div hover-show-complete-title">
-          <p className="componentItem-title"> { title } </p>
-        </div>*/}
-      </div>
-    </div>
-  );
+	return(
+		<div className="componentItem-box-container">
+			<div className="componentItem-box" onClick={() => { getMonitors(title) }}>
+				<div className="componentItem-icon component-icon-color">
+					<img src={folder} alt="folder" className="component_IMG-folder"/>
+				</div>
+				<div className="componentItem-title-div">
+					<p className="componentItem-title"> { title } </p>
+				</div>
+		{/*        <div className="componentItem-title-div hover-show-complete-title">
+				<p className="componentItem-title"> { title } </p>
+				</div>*/}
+			</div>
+		</div>
+	);
 }
 
 export default ComponentElement;
