861fe328ff186b38bd439ac4c9ebcd14cdec4314
danielglez4234 <gnzlzdaniel.task@gmail.com>
2022-05-18 13:12:29 +0100

    adding MUI grid material ui

--- src/components/stroreQuerys/SaveQuery.js
+++ src/components/stroreQuerys/SaveQuery.js
@@ -1,37 +1,41 @@
-// TODO: 
-// agregar un campo que muestre la fecha fin y fecha final de la query
-// buscar la forma de llamar al metodos constructURL
-
-
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import * as $ 						from 'jquery'
+import { useSelector } 				from 'react-redux';
 import {
 	insertQuery
 } from '../../services/services'
-import { 
+
+import {makeStyles}					from '@material-ui/core';
+import {
     Modal,
     Box,
     Grid,
+	Stack,
+	Button,
     Typography,
-    IconButton,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    Checkbox,
-    ListItemText,
     Backdrop,
     CircularProgress
 } from '@mui/material';
-import LoadingButton                from '@mui/lab/LoadingButton';
+import ArchiveIcon 					from '@mui/icons-material/Archive';
 import SaveIcon                     from '@mui/icons-material/Save';
+import LoadingButton                from '@mui/lab/LoadingButton';
+
 import PopUpMessage                 from '../../components/handleErrors/PopUpMessage';
-import {makeStyles}					from '@material-ui/core';
 import getGraphicoptions            from '../getGraphicoptions'
 
 
-
 const usesTyles = makeStyles({
-	saveQueryButton: {    
+	savebutton: {
+		'&:hover': {
+			background: '#e57070',
+		},
+	},
+	handlebutton: {
+		'&:hover': {
+			background: '#4b6180',
+		},
+	},
+	saveQueryButton: {
 		'&:hover': {
 			background: '#569d90',
 		},
@@ -39,216 +43,288 @@ const usesTyles = makeStyles({
 })
 
 
-function SaveQuery({open, handleClose, monitorsSelected, searchForm, constructURL}) {
+function SaveQuery({convertToUnix, constructURL, timeQuery}) {
+	const classes = usesTyles()
+	const monitor = useSelector(state => state.monitor)
+	const [msg, handleMessage] = PopUpMessage()
 
-    console.log("searchForm?.sampling", searchForm.sampling)
+	const [disabled, setDisabled] = useState(true)
+	const [openSaveQuery, setOpenSaveQuery] = useState(false)
 
-    const classes = usesTyles()
-    const monitorOptions = getGraphicoptions()
-    const [msg, handleMessage] = PopUpMessage()
     const [queryName, setQueryName] = useState("")
     const [queryDescription, setQueryDescription] = useState("")
     const [openBackDrop, setOpenBackDrop] = useState(false)
+    const [monitorList, setMonitorList] = useState([""]);
 
-    const backDropLoadOpen = () => { setOpenBackDrop(true) }
-    const backDropLoadClose = () => { setOpenBackDrop(false) }
+    const backDropLoadOpen = () => setOpenBackDrop(true)
+    const backDropLoadClose = () => setOpenBackDrop(false)
 
-
-    /*
-     * Show Warning message snackbar
-     */
-    const showWarningMessage = (message) => { 
-        handleMessage({
-            message: message,
-            type: "warning",
-            persist: false,
-            preventDuplicate: false
-        })
-    }
-
-
-    /*
-     * save query on data base
-     */
-    const onSubmit = () => {
-        if(queryName === "" || queryName === undefined){
-            showWarningMessage("Name field cannot be empty")
-        }
-        else{
-            backDropLoadOpen()
-            const params = constructURL(searchForm)
-            saveQuery(params)
-        }
+	const handleOpenSaveQuery = () => {
+		setOpenSaveQuery(true)
+		buildList()
 	}
-    
-    /*
-     * save query on data base 
-     */
-    const saveQuery = (payload) => { 
-        Promise.resolve( insertQuery(payload) )
-        .then(() =>{
-            setQueryName("")
-            setQueryDescription("")
-            handleMessage({
-                message: "Query save successfully!",
-                type: "success",
-                persist: false,
-                preventDuplicate: false
-            })
-        })
-        .catch((error) =>{
-            console.error(error)
-            const error_message = (error?.response?.message) ? error.response.message : "Unsupported Error"
-            const error_status = (error?.status) ? error.status : "Unkwon"
-            handleMessage({
-                message: "Error: " + error_message + " - Code " + error_status,
-                type: "error",
-                persist: true,
-                preventDuplicate: false
-            })
-        }).finally(() => {
-            backDropLoadClose()
-        })
-    }
-    
-    return (
-        <>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box className="save-query-modal">
-                    <Grid container spacing={0}>
-                        <Grid item xs={12} sm={12} md={12} className="save-query-title">
-                            Save Actual Query
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} className="save-query-box-title-name">
-                            <Grid
-                                container
-                                spacing={0}
-                                direction="row"
-                                justifyContent="flex-start"
-                                alignItems="center"
-                                className="save-query-input-title-name"
-                            >
-                                <Grid item xs={1} sm={1} md={1} className="save-query-input-title-name">
-                                    name
-                                </Grid>
-                                <Grid item xs={10} sm={10} md={10}>
-                                    <input 
-                                        className="save-query-input"
-                                        type="text" 
-                                        name="name"
-                                        placeholder="query_name"
-                                        value={queryName} 
-                                        onChange={(e) => {setQueryName(e.target.value)}}
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} className="save-query-box-title-name">
-                            <Grid
-                                container
-                                spacing={0}
-                                direction="row"
-                                justifyContent="flex-start"
-                                alignItems="center"
-                                className="save-query-input-title-name"
-                            >
-                                <Grid item md={2.5} className="save-query-input-title-name">
-                                    description 
-                                </Grid>
-                                <Grid item md={8.5}>
-                                    <input 
-                                        className="save-query-input description"
-                                        type="text" 
-                                        name="name"
-                                        placeholder="optional"
-                                        value={queryDescription} 
-                                        onChange={(e) => {setQueryDescription(e.target.value)}}
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} className="save-query-box-title-name save-query-box-title-date">
-                            <Grid
-                                container
-                                spacing={0}
-                                direction="column"
-                                justifyContent="flex-start"
-                                alignItems="left"
-                                className="save-query-input-title-name save-query-title-date"
-                            >
-                                <Grid item md={2} className="save-query-input-title-name">
-                                    <i>Begin date: { searchForm?.beginDate } </i>
-                                </Grid>
-                                <Grid item md={10}>
-                                    <i>End date: { searchForm?.endDate } </i>
-                                </Grid>
-                                <Grid item md={10}>
-                                    <i>Sampling: { (searchForm?.sampling !== "" && searchForm?.sampling !== "Default") ? searchForm.sampling  : 0 } </i>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} className="save-query-list-info-label">
-                            <Grid container spacing={0} sx={{backgroundColor: "#5a6370", borderBottom: "3px solid #85e1d0", padding: "3px 0px 3px 15px"}}>
-                                <Grid item md={12}>Monitors Settings</Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid container spacing={0} className="save-query-list-box">
-                            <Grid item xs={12} sm={12} md={12}>
-                                <table id="drop-area" className="save-query-table-monitor-list">
-                                    <tbody>
-                                        {monitorsSelected.map((value) => {
-                                            return (
-                                                <tr key={value.monitorData.id} className="save-query-table-tr">
-                                                    <td>
-                                                        {value.monitorData.magnitude}
-                                                    </td>
-                                                    <td>
-                                                        {value.component}
-                                                    </td>
-                                                    <td>
-                                                        {monitorOptions.unitType}
-                                                    </td>
-                                                    <td>
-                                                        {monitorOptions?.prefix}
-                                                    </td>
-                                                </tr>
-                                            );
-                                        })}
-                                    </tbody>
-                                </table>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} className="save-query-saveButton-box">
-                            <LoadingButton
-                                size="small"
-                                sx={{backgroundColor: '#569d90'}}
-                                className={classes.saveQueryButton}
-                                onClick={() => { 
-                                    onSubmit() 
-                                }}
-                                loadingPosition="start"
-                                startIcon={<SaveIcon />}
-                                variant="contained"
-                            >
-                                Save
-                            </LoadingButton>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Modal>
-            <Backdrop
-                sx={{ color: '#569d90', zIndex: (theme) => theme.zIndex.drawer + 13001 }}
-                open={openBackDrop}
-            >
-                <CircularProgress color="inherit" />
-            </Backdrop>
-        </>
-     );
-}
+	const handleCloseSaveQuery = () => setOpenSaveQuery(false)
 
+	/*
+	 * Show Warning message snackbar
+	 */
+	const showWarningMessage = (message) => {
+		handleMessage({
+			message: message,
+			type: "warning",
+			persist: false,
+			preventDuplicate: false
+		})
+	}
+
+
+	useEffect(() => {
+		const unixBeginDate = convertToUnix(timeQuery.beginDate)
+		const unixEndDate = convertToUnix(timeQuery.endDate)
+		if(monitor.length > 0 && timeQuery.beginDate !== "" && timeQuery.endDate !== "" && unixBeginDate < unixEndDate){
+			setDisabled(false)
+		}else{
+			setDisabled(true)
+		}
+	}, [monitor, timeQuery])
+
+
+	/*
+	 * save query on data base
+	 */
+	const onSubmit = () => {
+		try {
+			if(queryName === "" || queryName === undefined){
+				showWarningMessage("Name field cannot be empty")
+			}
+			else{
+				backDropLoadOpen()
+				saveQuery()
+			}
+		} catch (error) {
+			showWarningMessage(error)
+		}
+	}
+
+	/*
+	 * save query on data base
+	 */
+	const saveQuery = () => {
+		const payload = createPayload()
+		Promise.resolve(insertQuery(payload))
+		.then(() =>{
+			setQueryName("")
+			setQueryDescription("")
+			handleCloseSaveQuery()
+			handleMessage({
+				message: "Query save successfully!",
+				type: "success",
+				persist: false,
+				preventDuplicate: false
+			})
+		})
+		.catch((error) =>{
+			console.error(error)
+			const error_message = (error?.response?.message) ? error.response.message : "Unsupported Error"
+			const error_status = (error?.status) ? error.status : "Unkwon"
+			handleMessage({
+				message: "Error: " + error_message + " - Code " + error_status,
+				type: "error",
+				persist: true,
+				preventDuplicate: false
+			})
+		}).finally(() => {
+			backDropLoadClose()
+		})
+	}
+
+	/*
+	 * buil monitor data list
+	 */
+	const buildList = () => {
+		const monitorOptions = getGraphicoptions()
+		let list = []
+		for (let i = 0; i < monitor.length; i++) {
+			list.push({
+				component: monitor[i]?.component,
+				id: monitor[i]?.monitorData?.id,
+				magnitude: monitor[i]?.monitorData?.magnitude,
+				prefix: (monitorOptions?.prefix[i]) ? monitorOptions?.prefix[i] : "None",
+				unit: monitorOptions?.unitType[i]
+			});
+		}
+		setMonitorList(list)
+	}
+
+	const createPayload = () => {
+		const user = null
+		const name = queryName
+		const description = queryDescription
+		const query = constructURL(timeQuery)
+		return {name, description, user, query}
+	}
+
+
+    return(
+		<>
+			<Button
+				sx={{backgroundColor: '#e57070'}}
+				onClick={handleOpenSaveQuery}
+				className={classes.savebutton}
+				variant="contained"
+				startIcon={<ArchiveIcon />}
+				disabled={disabled}
+			>
+				Save Actual Query
+			</Button>
+
+		{/*
+			Modals
+		*/}
+
+			<Modal
+				open={openSaveQuery}
+				onClose={handleCloseSaveQuery}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box className="save-query-modal">
+					<Grid container spacing={0}>
+						<Grid item xs={12} sm={12} md={12} className="save-query-title">
+							Save Actual Query
+						</Grid>
+						<Grid item xs={12} sm={12} md={12} className="save-query-box-title-name">
+							<Grid
+								container
+								spacing={0}
+								direction="row"
+								justifyContent="flex-start"
+								alignItems="center"
+								className="save-query-input-title-name"
+							>
+								<Grid item xs={1} sm={1} md={1} className="save-query-input-title-name">
+									name
+								</Grid>
+								<Grid item xs={10} sm={10} md={10}>
+									<input
+										className="save-query-input"
+										type="text"
+										name="name"
+										placeholder="query_name"
+										value={queryName}
+										onChange={(e) => {setQueryName(e.target.value)}}
+									/>
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid item xs={12} sm={12} md={12} className="save-query-box-title-name">
+							<Grid
+								container
+								spacing={0}
+								direction="row"
+								justifyContent="flex-start"
+								alignItems="center"
+								className="save-query-input-title-name"
+							>
+								{/* TODO: cambiar ed 3 2.5 */}
+								<Grid item md={3} className="save-query-input-title-name">
+									description
+								</Grid>
+								<Grid item md={8}>
+									<input
+										className="save-query-input description"
+										type="text"
+										name="name"
+										placeholder="optional"
+										value={queryDescription}
+										onChange={(e) => {setQueryDescription(e.target.value)}}
+									/>
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid item xs={12} sm={12} md={12} className="save-query-box-title-name save-query-box-title-date">
+							<Grid
+								container
+								spacing={0}
+								direction="column"
+								justifyContent="flex-start"
+								alignItems="left"
+								className="save-query-input-title-name save-query-title-date"
+							>
+								<Grid item md={2} className="save-query-input-title-name">
+									<i>Begin date: { timeQuery?.beginDate } </i>
+								</Grid>
+								<Grid item md={10}>
+									<i>End date: { timeQuery?.endDate } </i>
+								</Grid>
+								<Grid item md={10}>
+									<i>Sampling: { (timeQuery?.sampling !== "" && timeQuery?.sampling !== "Default") ? timeQuery.sampling  : 0 } microseconds</i>
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid item xs={12} sm={12} md={12} className="save-query-list-info-label">
+							<Grid container spacing={0} sx={{backgroundColor: "rgb(40, 46, 57)", borderBottom: "3px solid #85e1d0", padding: "3px 0px 3px 15px"}}>
+								<Grid item md={12}>Monitors Settings</Grid>
+							</Grid>
+						</Grid>
+						<Grid container spacing={0} className="save-query-list-box">
+							<Grid item xs={12} sm={12} md={12}>
+								<table id="drop-area" className="save-query-table-monitor-list">
+									<tbody>
+										{
+											(monitorList === "") ? <td></td>
+											:
+											monitorList.map((value, index) => {
+												return (
+													<tr key={index} className="save-query-table-tr">
+														<td>
+															<div className="save-query-table-item-header">
+																<p className="sv-component">{value.component}</p>
+																<p className="sv-prefix">
+																	{(value.prefix === "None" || value.prefix === "") ? "--" : value.prefix} 
+																</p>
+																<p className="sv-unit"> 
+																	{(value.unit === "None" || value.unit === "") ? "--" : value.unit}
+																</p>
+															</div>
+															<div className="save-query-table-item-title">
+																{value.magnitude}
+															</div>
+														</td>
+													</tr>
+												);
+											})
+										}
+									</tbody>
+								</table>
+							</Grid>
+						</Grid>
+						<Grid item xs={12} sm={12} md={12} className="save-query-saveButton-box">
+							<LoadingButton
+								size="small"
+								sx={{backgroundColor: '#569d90'}}
+								className={classes.saveQueryButton}
+								onClick={() => {
+									onSubmit()
+								}}
+								loadingPosition="start"
+								startIcon={<SaveIcon />}
+								variant="contained"
+							>
+								Save
+							</LoadingButton>
+						</Grid>
+					</Grid>
+				</Box>
+			</Modal>
+			<Backdrop
+				sx={{ color: '#569d90', zIndex: (theme) => theme.zIndex.drawer + 13001 }}
+				open={openBackDrop}
+			>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+	</>
+
+    );
+}
 export default SaveQuery;
---
+++ src/components/stroreQuerys/handleQuerys/ViewHandleQuery.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+
+import {makeStyles}					from '@material-ui/core';
+import { 
+    Modal,
+    Box,
+    Grid,
+	Stack,
+	Button,
+    Typography,
+} from '@mui/material';
+import ArchiveIcon 					from '@mui/icons-material/Archive';
+import SaveIcon                     from '@mui/icons-material/Save';
+import InventoryIcon				from '@mui/icons-material/Inventory';
+import LoadingButton                from '@mui/lab/LoadingButton';
+
+import QueryTable 					from './QueryTable';
+
+
+const usesTyles = makeStyles({
+	savebutton: {    
+		'&:hover': {
+			background: '#e57070',
+		},
+	},
+	handlebutton: {    
+		'&:hover': {
+			background: '#4b6180',
+		},
+	},
+	saveQueryButton: {    
+		'&:hover': {
+			background: '#569d90',
+		},
+	}
+})
+
+
+function ViewHandleQuery({constructURL}) {
+	const classes = usesTyles()
+	const [openViewQuery, setOpenViewQuery] = useState(false)
+
+	const handleOpenViewQuery = () => setOpenViewQuery(true)
+	const handleCloseSaveQuery = () => setOpenViewQuery(false)
+
+
+    return(
+		<>
+			<Button
+				sx={{backgroundColor: '#4b6180', height: 60}} 
+				className={classes.handlebutton}
+				variant="contained"  
+				startIcon={<InventoryIcon />}
+				onClick={() => {handleOpenViewQuery()}}
+			>
+				View Store Querys
+			</Button>
+
+		{/*
+			Modals
+		*/}
+
+			<Modal
+				open={openViewQuery}
+				onClose={handleCloseSaveQuery}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<Box className="view-query-modal">
+					<Grid container spacing={0}>
+						<Grid item xs={12} sm={12} md={12} className="save-query-title">
+							View Store Querys
+						</Grid>
+
+						{/* <Grid container spacing={0}> */}
+							<Grid item xs={12} sm={12} md={12} className="save-query-title">
+								{
+									<QueryTable
+										openViewQuery={openViewQuery}
+									/>
+								}
+							</Grid>
+						{/* </Grid> */}
+					</Grid>
+				</Box>
+			</Modal>
+	</>
+
+    );
+}
+export default ViewHandleQuery;
