ff07446e10db499c41425f0838d2a3f749733016
danielglez4234 <gnzlzdaniel.task@gmail.com>
2022-07-08 14:25:24 +0100

    refactor arrageMonitors data set to global funct

--- src/components/Sections/PerformSearchSection/manageMonitorData.js
+++ src/components/Sections/PerformSearchSection/manageMonitorData.js
@@ -1,3 +1,7 @@
+import { fnIsArray } from '../../standarFunctions'
+
+// TODO: REFACTOR: hace esta función más eficiente
+
 /*
  * Arrange the data received from the Backend according to the needs of the Frontend
  */
@@ -9,19 +13,39 @@ export const arrageMonitors =  (data) => {
         const options = item.options
         let monitorData
         if(item?.id_magnitude_description){
+            // generate monitor element
             monitorData = item.id_magnitude_description
-            monitorList.push({component_id, name, ...monitorData, options})
+            monitorList.push({
+                component_id, 
+                name, 
+                ...monitorData, 
+                options
+            })
         }
         else if(item?.id_monitor_description){
+            // generate monitor element
             monitorData = item.id_monitor_description
             item.options["prefix"] 	= item.prefix
             item.options["unit"] 	= item.unit
             item.options["decimal"] = item.decimal
-            item.options["pos"] 	= item.pos.slice(1, -1) // TODO: REFACTOR: esto no debería ser necesario
-            monitorList.push({component_id, name, ...monitorData, options})
+            // TODO: REFACTOR: el slice no debería ser necesario
+            item.options["pos"] 	= (fnIsArray(item.id_monitor_description.type)) ? item.pos.slice(1, -1) : ""
+            monitorList.push({
+                component_id, 
+                name, 
+                ...monitorData, 
+                options
+            })
         }
         else{
-            monitorList.push({id: component_id, name, magnitude: "STATE", type: "state", options})
+            // generate state element
+            monitorList.push({
+                id: component_id, 
+                name, 
+                magnitude: "STATE", 
+                type: "state", 
+                options
+            })
         }
     })
     return monitorList
